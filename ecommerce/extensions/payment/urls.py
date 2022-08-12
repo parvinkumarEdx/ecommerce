e88d63fb09d5a5f603dd5bb95355bb67b3983a54
parvinkumarEdx <111120136+parvinkumarEdx@users.noreply.github.com>
2022-08-12 15:37:13 +0530

    Update urls.py

--- ecommerce/extensions/payment/urls.py
+++ ecommerce/extensions/payment/urls.py
@@ -1,9 +1,8 @@
-
-
 from django.conf import settings
+
 from django.conf.urls import include, url
 
-from ecommerce.extensions.payment.views import PaymentFailedView, SDNFailure, cybersource, paypal, stripe
+from ecommerce.extensions.payment.views import PaymentFailedView, SDNFailure, cybersource, paypal, stripe, razorpay
 
 CYBERSOURCE_APPLE_PAY_URLS = [
     url(r'^authorize/$', cybersource.CybersourceApplePayAuthorizationView.as_view(), name='authorize'),
@@ -27,14 +26,20 @@ STRIPE_URLS = [
     url(r'^submit/$', stripe.StripeSubmitView.as_view(), name='submit'),
 ]
 
+RAZORPAY_URLS = [
+    url(r'^execution/$', razorpay.RazorpayExecutionView.as_view(), name='razorpay-execution'),
+    url(r'^cancel/$', razorpay.RazorpayCancelView.as_view(), name='razorpay-cancel'),
+]
+
 urlpatterns = [
     url(r'^cybersource/', include((CYBERSOURCE_URLS, 'cybersource'))),
     url(r'^error/$', PaymentFailedView.as_view(), name='payment_error'),
     url(r'^paypal/', include((PAYPAL_URLS, 'paypal'))),
     url(r'^sdn/', include((SDN_URLS, 'sdn'))),
     url(r'^stripe/', include((STRIPE_URLS, 'stripe'))),
+    url(r'^razorpay/', include((RAZORPAY_URLS, 'razorpay'))),
 ]
 
-for payment_processor_name, urls_module in settings.EXTRA_PAYMENT_PROCESSOR_URLS.items():
-    processor_url = url(r'^{}/'.format(payment_processor_name), include((urls_module, payment_processor_name)))
+for payment_processor_name, urls_module in settings.EXTRA_PAYMENT_PROCESSOR_URLS.items():	
+    processor_url = url(r'^{}/'.format(payment_processor_name), include((urls_module, payment_processor_name)))	
     urlpatterns.append(processor_url)
